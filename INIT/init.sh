d77afdd5258caa6ab135ce26d159df7b657256a7
ihgazni2 <ihgazni2010@hotmail.com>
2019-08-27 04:30:39 +0000

    first commit

--- INIT/init.sh
+++ INIT/init.sh
@@ -5,18 +5,18 @@ git init
 
 if [$scheme == "https"];then
     echo "$scheme is https"
-    git remote add origin https://github.com:ihgazni2/pyminiproj.git
-    git remote add origin-git git@github.com:ihgazni2/pyminiproj.git
-    git remote add origin-https https://github.com:ihgazni2/pyminiproj.git
+    eval `git remote add origin https://github.com:ihgazni2/pyminiproj.git`
+    eval `git remote add origin-git git@github.com:ihgazni2/pyminiproj.git`
+    eval `git remote add origin-https https://github.com:ihgazni2/pyminiproj.git`
 fi
 
 if [$scheme == "git"];then
     echo "$scheme is git"
     eval `ssh-agent`
-    ssh-add
-    git remote add origin git@github.com:ihgazni2/pyminiproj.git
-    git remote add origin-git git@github.com:ihgazni2/pyminiproj.git
-    git remote add origin-https https://github.com:ihgazni2/pyminiproj.git
+    eval `ssh-add`
+    eval `git remote add origin git@github.com:ihgazni2/pyminiproj.git`
+    eval `git remote add origin-git git@github.com:ihgazni2/pyminiproj.git`
+    eval `git remote add origin-https https://github.com:ihgazni2/pyminiproj.git`
 fi
 
 git config --local user.name ihgazni2
